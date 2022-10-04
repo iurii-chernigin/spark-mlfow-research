2e4cf38aad77405741f2e1bdba050b879b30ec5a
iurii-chernigin <chernigin.y@gmail.com>
2022-10-05 00:06:30 +0400

    Implemet mlflow log: params, metrics, and model

--- PySparkFit.py
+++ PySparkFit.py
@@ -9,6 +9,10 @@ from pyspark.sql import SparkSession
 from pyspark.ml.classification import GBTClassifier
 import mlflow
 
+os.environ['MLFLOW_S3_ENDPOINT_URL'] = ''
+os.environ['AWS_ACCESS_KEY_ID'] = ''
+os.environ['AWS_SECRET_ACCESS_KEY'] = ''
+
 mlflow.set_tracking_uri('https://mlflow.lab.karpov.courses')
 mlflow.set_experiment(experiment_name = 'j-chernigin-8')
 
@@ -27,6 +31,7 @@ def build_pipeline(train_alg):
                                       outputCol="car_class_index")
     features = ["age", "sex_index", "car_class_index", "driving_experience",
                 "speeding_penalties", "parking_penalties", "total_car_accident"]
+    mlflow.log_param('features', features)
     assembler = VectorAssembler(inputCols=features, outputCol='features')
     return Pipeline(stages=[sex_indexer, car_class_indexer, assembler, train_alg])
 
@@ -50,7 +55,15 @@ def optimization(pipeline, gbt, train_df, evaluator):
                                evaluator=evaluator,
                                trainRatio=0.8)
     models = tvs.fit(train_df)
-    return models.bestModel
+    best_model = models.bestModel
+
+    mlflow.log_param('maxDepth', best_model.stages[-1].getMaxDepth())
+    mlflow.log_param('maxBins', best_model.stages[-1].getMaxBins())
+    mlflow.log_param('maxIter', best_model.stages[-1].getMaxIter())
+    for i in range(0, len(best_model.stages)):
+        mlflow.log_param(f'stage_{i}', best_model.stages[i].__class__.__name__)
+
+    return best_model
 
 
 def process(spark, train_path, test_path):
@@ -67,15 +80,17 @@ def process(spark, train_path, test_path):
     test_df = spark.read.parquet(test_path)
 
     mlflow.log_param('input_columns', train_df.columns)
+    mlflow.log_param('target', LABEL_COL)
 
     gbt = GBTClassifier(labelCol=LABEL_COL)
     pipeline = build_pipeline(gbt)
 
     model = optimization(pipeline, gbt, train_df, evaluator)
+    
     predict = model.transform(test_df)
-
     evaluate_model(evaluator, predict, ['f1', 'weightedPrecision', 'weightedRecall', 'accuracy'])
     
+    mlflow.spark.log_model(model, artifact_path='j-chernigin-8', registered_model_name='j-chernigin-8')
     print('Best model saved')
     mlflow.end_run()
 
