5444b78aa50816a1402ca2f58aee1140b068e096
iurii-chernigin <chernigin.y@gmail.com>
2022-10-05 21:28:51 +0400

    Separate scripts to directories and load template script for mlflow model load

---
+++ load-and-apply/PySparkPredict.py
@@ -0,0 +1,41 @@
+import argparse
+import os
+
+from pyspark.sql import SparkSession
+
+import mlflow
+
+
+def process(spark, data_path, result):
+    """
+    Основной процесс задачи.
+
+    :param spark: SparkSession
+    :param data_path: путь до датасета
+    :param result: путь сохранения результата
+    """
+    # TODO Ваш код
+
+
+def main(data, result):
+    spark = _spark_session()
+    process(spark, data, result)
+
+
+def _spark_session():
+    """
+    Создание SparkSession.
+
+    :return: SparkSession
+    """
+    return SparkSession.builder.appName('PySparkPredict').getOrCreate()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data', type=str, default='data.parquet', help='Please set datasets path.')
+    parser.add_argument('--result', type=str, default='result', help='Please set result path.')
+    args = parser.parse_args()
+    data = args.data
+    result = args.result
+    main(data, result)
